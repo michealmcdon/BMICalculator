329875558edd47c573892ec711e4f9d25d97fb60
Micheál <Mickeymac89@gmail.com>
2018-08-23 17:59:53 +0100

    App running well, need some bug fix.

--- BMICalculator/MainPage.xaml.cs
+++ BMICalculator/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace BMICalculator
@@ -22,9 +24,31 @@ namespace BMICalculator
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Windows.Storage.ApplicationDataContainer localSettings =
+            Windows.Storage.ApplicationData.Current.LocalSettings;
+        Windows.Storage.StorageFolder localFolder =
+            Windows.Storage.ApplicationData.Current.LocalFolder;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            
+        }
+
+        private void ButtonEntry_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            {
+                double weight = Double.Parse(WeightEntry.Text);
+                localSettings.Values["weight"] = weight;
+                double height = Double.Parse(HeightEntry.Text);
+                localSettings.Values["height"] = height;
+                double BMI = weight / (height * height);
+                App.BMICalc = BMI;
+                localSettings.Values["BMI"] = BMI;
+                this.Frame.Navigate(typeof(Result), null);
+            } catch (Exception Error){}
         }
     }
 }
---
+++ BMICalculator/Result.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BMICalculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Result : Page
+    {
+        public Result()
+        {
+            this.InitializeComponent();
+            showBMIResults();
+        }
+
+        private void showBMIResults()
+        {
+            Results.Text = "" + String.Format("{0:00.00}", App.BMICalc);
+            if (App.BMICalc < 20)
+            {
+                YouAre.Text = "You are too skinny... get a burger!";
+            }
+            else if (App.BMICalc < 25)
+            {
+                YouAre.Text = "You're healthy... Good job!";
+            }
+            else if (App.BMICalc <= 30)
+            {
+                YouAre.Text = "You are getting fat, eat a salad!";
+            }
+            else if (App.BMICalc < 40)
+            {
+                YouAre.Text = "You are getting really fat, eat alot less!";
+            }
+            else if (App.BMICalc > 40)
+            {
+                YouAre.Text = "You are extremely Obese, stop eating and start running!";
+            }
+
+
+        }
+
+        private void ButtonEntry_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+    }
+}
+
